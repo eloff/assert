475f5cb4c261cc082fecd9df365c012f76cc4918
Eloff <dan.eloff@gmail.com>
2019-07-10 08:51:45 -0700

    switch from merry to pkg/errors

--- assert.go
+++ assert.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/pkg/errors"
 
-	"github.com/ansel1/merry"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
@@ -18,7 +18,7 @@ func (t *T) CheckError(actual error, expected interface{}) bool {
 	switch val := expected.(type) {
 	case nil:
 		if hadError {
-			t.T.Errorf("%s: unexpected error %s", t.Name, merry.Details(actual))
+			t.T.Errorf("%s: unexpected error %+v", t.Name, actual)
 		}
 	case bool:
 		if val {
@@ -26,25 +26,22 @@ func (t *T) CheckError(actual error, expected interface{}) bool {
 				t.T.Errorf("%s: expected an error, but call succeeded", t.Name)
 			}
 		} else if hadError {
-			t.T.Errorf("%s: unexpected error %s", t.Name, merry.Details(actual))
+			t.T.Errorf("%s: unexpected error %+v", t.Name, actual)
 		}
 	case error:
-		if !merry.Is(actual, val) && (actual == nil || actual.Error() != val.Error()) {
-			details := "<nil>"
-			if actual != nil {
-				details = merry.Details(actual)
-			}
-			t.T.Errorf("%s: expected error %s but got %s", t.Name, val, details)
+		cause := errors.Cause(actual)
+		if cause.Error() != val.Error() {
+			t.T.Errorf("%s: expected error %s but got %+v", t.Name, val.Error(), actual)
 		}
 	case string:
 		if val != "" {
 			if !hadError {
 				t.T.Errorf("%s: expected an error containing %s, but call succeeded", t.Name, val)
 			} else if !strings.Contains(actual.Error(), val) {
-				t.T.Errorf("%s: expected an error containing %s, but got %s", t.Name, val, merry.Details(actual))
+				t.T.Errorf("%s: expected an error containing %s, but got %+v", t.Name, val, actual)
 			}
 		} else if hadError {
-			t.T.Errorf("%s: unexpected error %s", t.Name, merry.Details(actual))
+			t.T.Errorf("%s: unexpected error %+v", t.Name, actual)
 		}
 	default:
 		panic("fatal error: expected must be an error or a string or nil")
